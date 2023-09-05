754c1615ddd55ea365c702c5d9a74650fa40116f
Nathan Debaz <nathandebaz266@gmail.com>
2023-09-05 11:24:04 -0300

    Drag an drop and validating

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { Player } from "@lottiefiles/react-lottie-player";
 import folder from "./assets/folder.json";
 import airplay from "./assets/airplay.json";
+import check from "./assets/check.json";
+import error from "./assets/error.json";
+import loadingLottie from "./assets/loading.json";
 import { useEffect, useRef, useState } from "react";
 
 const App = () => {
   const folderRef = useRef<any>();
   const airplayRef = useRef<any>();
   const inputRef = useRef<any>();
+  const imageRef = useRef<any>();
   const [selectedImage, setSelectedImage] = useState<any>();
   const [dragActive, setDragActive] = useState(false);
+  const [imageWidth, setImageWidth] = useState<number | null>(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setInterval(() => {
@@ -16,84 +22,170 @@ const App = () => {
     }, 1000);
   }, []);
 
-  const handleDrag = (event: any) => {
+  const handleDragOver = (event: any) => {
     event.preventDefault();
     event.stopPropagation();
-    if (event.type === "dragenter" || event.type === "dragover") {
-      setDragActive(true);
-      console.log("drag enter");
-    } else if (event.type === "dragleave") {
-      setDragActive(false);
-      console.log("drag leave");
-    }
+  };
+
+  const handleDragEnter = (event: any) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    setDragActive(true);
+  };
+
+  const handleDragLeave = (event: any) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    setDragActive(false);
   };
 
   const handleDrop = (event: any) => {
+    console.log("Drop");
+
     event.preventDefault();
     event.stopPropagation();
+
     setDragActive(false);
-    if (event.dataTransfer.files && event.dataTransfer.files[0]) {
-      // at least one file has been dropped so do something
-      // handleFiles(e.dataTransfer.files);
-      console.log(event.dataTransfer.files[0]);
+
+    const { files } = event.dataTransfer;
+
+    if (files && files.length) {
+      setSelectedImage(URL.createObjectURL(files[files.length - 1]));
     }
   };
 
   return (
     <div
-      onDragEnter={handleDrag}
+      onDragOver={handleDragOver}
+      onDragEnter={handleDragEnter}
+      onDragLeave={handleDragLeave}
       onDrop={handleDrop}
-      className="h-screen w-screen flex flex-col justify-center items-center bg-background-light text-secondary space-y-4"
+      className={`h-screen w-screen flex flex-col justify-center items-center space-y-4 ${
+        dragActive ? "bg-background-dark" : "bg-background-light"
+      }`}
     >
-      <Player
-        ref={airplayRef}
-        speed={0.7}
-        src={airplay}
-        style={{ height: "10rem", width: "10rem" }}
-      />
-      <p>Drop your picture</p>
-      <div className="w-full flex justify-center items-center space-x-2 text-background-dark">
-        <div className="h-[2px] w-1/5 bg-background-dark" />
-        <p>or</p>
-        <div className="h-[2px] w-1/5 bg-background-dark" />
-      </div>
-      <p>Select it locally</p>
-      <input
-        ref={inputRef}
-        type="file"
-        className="hidden"
-        onChange={(event) =>
-          setSelectedImage(
-            event.currentTarget.files
-              ? URL.createObjectURL(
-                  event.currentTarget.files[
-                    event.currentTarget.files.length - 1
-                  ]
-                )
-              : undefined
-          )
-        }
-      />
-      <div
-        onMouseEnter={() => {
-          folderRef.current.setPlayerDirection(1);
-          folderRef.current.play();
-        }}
-        onMouseLeave={() => {
-          folderRef.current.setPlayerDirection(-1);
-          folderRef.current.play();
-        }}
-        onClick={() => inputRef.current.click()}
-        className="cursor-pointer"
-      >
+      {!selectedImage && !loading && (
+        <>
+          <Player
+            ref={airplayRef}
+            speed={0.7}
+            src={airplay}
+            style={{ height: "10vw", width: "10vw" }}
+          />
+          <p
+            className={`text-[1vw] ${
+              dragActive ? "text-white" : "text-secondary"
+            }`}
+          >
+            Drop your picture
+          </p>
+          <div
+            className={`w-full flex justify-center items-center space-x-2 ${
+              dragActive ? "text-background-light" : "text-background-dark"
+            }`}
+          >
+            <div
+              className={`h-[2px] w-1/5 ${
+                dragActive ? "bg-background-light" : "bg-background-dark"
+              }`}
+            />
+            <p className="text-[1vw]">or</p>
+            <div
+              className={`h-[2px] w-1/5 ${
+                dragActive ? "bg-background-light" : "bg-background-dark"
+              }`}
+            />
+          </div>
+          <p
+            className={`text-[1vw] ${
+              dragActive ? "text-white" : "text-secondary"
+            }`}
+          >
+            Select it locally
+          </p>
+          <input
+            ref={inputRef}
+            type="file"
+            className="hidden"
+            onChange={(event) =>
+              setSelectedImage(
+                event.currentTarget.files
+                  ? URL.createObjectURL(
+                      event.currentTarget.files[
+                        event.currentTarget.files.length - 1
+                      ]
+                    )
+                  : undefined
+              )
+            }
+          />
+          <div
+            onMouseEnter={() => {
+              folderRef.current.setPlayerDirection(1);
+              folderRef.current.play();
+            }}
+            onMouseLeave={() => {
+              folderRef.current.setPlayerDirection(-1);
+              folderRef.current.play();
+            }}
+            onClick={() => inputRef.current.click()}
+            className="cursor-pointer"
+          >
+            <Player
+              ref={folderRef}
+              keepLastFrame
+              src={folder}
+              style={{ height: "10vw", width: "10vw" }}
+            />
+          </div>
+        </>
+      )}
+      {selectedImage && !loading && (
+        <div
+          className="relative h-3/4 flex justify-center"
+          style={{ width: imageWidth ?? "100%" }}
+          onMouseOver={() => setImageWidth(imageRef.current?.offsetWidth)}
+        >
+          <button
+            className="absolute h-1/5 w-full bg-red-500 opacity-0 hover:opacity-100 bg-opacity-30 transition-all duration-200"
+            onClick={() => setSelectedImage(null)}
+          >
+            <Player
+              autoplay
+              loop
+              src={error}
+              style={{ height: "auto", width: "15%" }}
+            />
+          </button>
+          <img
+            ref={imageRef}
+            className="h-full"
+            src={selectedImage}
+            alt="Selected Image"
+          />
+          <button
+            className="absolute bottom-0 h-4/5 w-full bg-primary opacity-0 hover:opacity-100 bg-opacity-30 transition-all duration-200"
+            onClick={() => setLoading(true)}
+          >
+            <Player
+              autoplay
+              loop
+              src={check}
+              style={{ height: "auto", width: "30%" }}
+            />
+          </button>
+        </div>
+      )}
+      {loading && (
         <Player
-          ref={folderRef}
-          keepLastFrame
-          src={folder}
-          style={{ height: "10rem", width: "10rem" }}
+          autoplay
+          loop
+          src={loadingLottie}
+          style={{ height: "15vw", width: "15vw" }}
         />
-      </div>
-      {selectedImage && <img src={selectedImage} alt="Selected Image" />}
+      )}
     </div>
   );
 };
